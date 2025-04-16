ad557724bda5203433c6a140501e98e849a41bc4
Vasath <git@gmail.com>
2025-04-16 11:34:42 +0530

    Commit

--- src/test/java/stepDefinations/Hooks.java
+++ src/test/java/stepDefinations/Hooks.java
@@ -8,7 +8,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import base.TestBase;
-
+import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.Scenario;
 
@@ -19,13 +20,17 @@ public class Hooks {
 		TestBase.initDriver();
 	}
 
-	@AfterTest
-	public void tearDown(Scenario scenario) {
+	@After
+	public static void screenshot(Scenario scenario) {
 		if (scenario.isFailed()) {
 			TakesScreenshot screen = (TakesScreenshot) TestBase.getDriver();
 			byte[] imgBytes = screen.getScreenshotAs(OutputType.BYTES);
 			scenario.attach(imgBytes, "image/png", "ScreenImage");
 		}
+		
+	}
+	@AfterAll
+	public static void closeBrowser() {
 		TestBase.tearDown();
 	}
 
